c7de9b3efa554a3707bb947dd8c3076832be528d
ergodice <kamepower@outlook.jp>
2026-06-20 12:49:46 +0900

    fix: prefer units option does not working correctry

--- lua/statuscol-oil/size.lua
+++ lua/statuscol-oil/size.lua
@@ -5,31 +5,41 @@ local state = require("statuscol-oil.state")
 
 local units = { "", "K", "M", "G", "T" }
 
-local function set_units(num)
+local function get_digits_width(num)
+    local digits = 1
+    if num >= 1000 then
+        digits = 3
+    elseif num >= 10 then
+        digits = 2
+    end
+    return digits
+end
+
+local function set_units(num, width)
+    -- get units index
 	local unit = 1
 
 	while num >= 1000 and unit < #units do
 		num = num / 1000
 		unit = unit + 1
 	end
+    if unit == 1 then
+        return (string.format("%.0f", num) .. units[unit])
+    end
 
-	if num >= 100 then
-		return string.format("%.0f%s", num, units[unit])
-	elseif num >= 10 then
-		return string.format("%.1f%s", num, units[unit])
-	else
-		return string.format("%.2f%s", num, units[unit])
-	end
+    -- get units in width
+    local int_width = get_digits_width(num)
+    local decimal_width = width - int_width - #"." - #units[unit]
+    return (string.format("%." .. decimal_width .. "f", num) .. units[unit])
 end
 
 local function format_number(num, width, prefer_units)
 	width = width or 5
 
-	local plain = tostring(math.floor(num))
-	local united = set_units(num, width)
+    local united = set_units(num, width)
+    local plain = tostring(math.floor(num))
 
 	local result
-
 	if prefer_units then
 		result = united
 	else
@@ -43,6 +53,8 @@ local function format_number(num, width, prefer_units)
 	return util.right_align(result, width)
 end
 
+M.format_num = format_number
+
 M.size = function(args)
 	if not util.is_oil_buffer() or args.empty then
 		return ""
@@ -51,7 +63,7 @@ M.size = function(args)
 	local ctx = util.get_context(args.buf, args.lnum)
 
 	if ctx and ctx.stat and ctx.stat.size then
-		return format_number(ctx.stat.size, state.opts.size_width, state.opts.prefer_units)
+		return format_number(ctx.stat.size, state.opts.size_width, state.opts.size_prefer_units)
 	end
 
 	return string.rep("-", state.opts.size_width)
